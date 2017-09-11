104889751d6bb412ff8c8a4ac68e9e6c25f1b029
jasmine Magdi <jesojolay@yahoo.com>
2017-09-11 16:23:51 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,6 +3,13 @@ function getIt(){
 $("p").on("click",function(){
     alert("Hey!");
 });
+
+function frameIt() {
+    $("img").on("load",function(){
+        
+    });
+}
+
 }
 $(document).ready(function(){
 
