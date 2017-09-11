c572d07c98e597b7fe0a0c1ba0e54c8ab7b75444
jasmine Magdi <jesojolay@yahoo.com>
2017-09-11 16:35:39 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,32 +1,32 @@
 //define functions here
-function getIt(){
-$("p").on("click",function(){
-    alert("Hey!");
-});
+function getIt() {
+    $("p").on("click", function() {
+        alert("Hey!");
+    });
 }
 
 function frameIt() {
-    $("img").on("load",function(){
+    $("img").on("load", function() {
         $(this).addClass("tasty");
     });
 }
 
-function pressIt(){
-    $("#typing").on("keydown",function(e){
-        if(e.which === 71){
+function pressIt() {
+    $("#typing").on("keydown", function(e) {
+        if (e.which === 71) {
             alert("You Pressed G!");
         }
     });
 }
 
-function submitIt(){
-    $("form").on("submit",function(){
+function submitIt() {
+    $("form").on("submit", function() {
         alert("Your form is going to be submitted now.");
     });
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-// call functions here
+    // call functions here
 
 });
