ea83ec19b02b25a7ae4c51da09840b69a7a60d19
jasmine Magdi <jesojolay@yahoo.com>
2017-09-11 16:34:25 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,14 +3,22 @@ function getIt(){
 $("p").on("click",function(){
     alert("Hey!");
 });
+}
 
 function frameIt() {
     $("img").on("load",function(){
-        
+        $(this).addClass("tasty");
     });
 }
 
+function pressIt(){
+    $("#typing").on("keydown",function(e){
+        if(e.which === 71){
+            alert("You Pressed G!");
+        }
+    });
 }
+
 $(document).ready(function(){
 
 // call functions here
